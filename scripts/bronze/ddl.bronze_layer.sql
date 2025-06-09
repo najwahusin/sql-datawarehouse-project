635e6f8529aba08dd3fc535ce2a1326bb49d9b9a
najwahusin <106375779+najwahusin@users.noreply.github.com>
2025-06-09 22:07:07 +0800

    Update ddl.bronze_layer.sql
    Changed column names

--- scripts/bronze/ddl.bronze_layer.sql
+++ scripts/bronze/ddl.bronze_layer.sql
@@ -9,7 +9,7 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_key NVARCHAR(50),
 	cst_firstname NVARCHAR(50),
 	cst_lastname NVARCHAR(50),
-	cst_material_status NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
 	cst_gndr NVARCHAR(50),
 	cst_create_date DATE
 );
@@ -19,7 +19,7 @@ IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
 CREATE TABLE bronze.crm_prd_info (
 	prd_id INT,
 	prd_key NVARCHAR(50),
-	prd_nme NVARCHAR(50),
+	prd_name NVARCHAR(50),
 	prd_cost INT,
 	prd_line NVARCHAR(50),
 	prd_start_dt DATETIME,
