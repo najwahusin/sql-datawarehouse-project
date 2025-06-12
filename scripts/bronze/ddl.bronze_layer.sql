0176c9fee83b5f87df9e730c66b3629967dd7407
najwahusin <106375779+najwahusin@users.noreply.github.com>
2025-06-12 09:16:57 +0800

    Update ddl.bronze_layer.sql

--- scripts/bronze/ddl.bronze_layer.sql
+++ scripts/bronze/ddl.bronze_layer.sql
@@ -40,12 +40,6 @@ CREATE TABLE bronze.crm_sales_details (
 	sls_price INT
 );
 
-IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
-	DROP TABLE bronze.erp_loc_a101;
-CREATE TABLE bronze.erp_loc_a101 (
-	cid NVARCHAR(50),
-	cntry NVARCHAR(50)
-);
 
 IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
@@ -55,9 +49,17 @@ CREATE TABLE bronze.erp_cust_az12 (
 	gndr NVARCHAR(50)
 );
 
-IF OBJECT_ID ('bronze.erp_px_cat_giv2', 'U') IS NOT NULL
-	DROP TABLE bronze.erp_px_cat_giv2;
-CREATE TABLE bronze.erp_px_cat_giv2 (
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101 (
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50)
+);
+
+
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
 	id NVARCHAR(50),
 	cat NVARCHAR(50),
 	subcat NVARCHAR(50),
